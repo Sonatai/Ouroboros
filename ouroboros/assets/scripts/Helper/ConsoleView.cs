9f961e11f02351d1148841125366eb878775da36
vandermingo <danneydan@gmx.at>
2019-03-26 10:21:11 +0100

    implement console

---
+++ ouroboros/assets/scripts/Helper/ConsoleView.cs
@@ -0,0 +1,110 @@
+﻿/// 
+/// Marshals events and data between ConsoleController and uGUI.
+/// 
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class ConsoleView : MonoBehaviour
+{
+    ConsoleController console = new ConsoleController();
+    GameStateController gamestate;
+    bool didShow = false;
+
+    public GameObject ViewContainer; //Container for console view, should be a child of this GameObject
+    public Text logTextArea;
+    public InputField inputField;
+    
+    //====================================================
+    void OnVisibilityChanged(bool visible)
+    {
+        SetVisibility(visible);
+    }
+
+    void OnLogChanged(string[] newLog)
+    {
+        UpdateLogStr(newLog);
+    }
+    //====================================================
+
+    void Start()
+    {
+        gamestate = GameObject.FindObjectOfType(typeof(GameStateController)) as GameStateController;
+
+        if (console != null)
+        {
+             console.visibilityChanged += OnVisibilityChanged;
+             console.logChanged += OnLogChanged;
+        }
+        UpdateLogStr(console.log);
+    }
+    ~ConsoleView()
+    {
+        console.visibilityChanged -= OnVisibilityChanged;
+        console.logChanged -= OnLogChanged;
+    }
+
+    void UpdateLogStr(string[] newLog)
+    {
+        if (newLog == null)
+        {
+            logTextArea.text = "";
+        }
+        else
+        {
+            logTextArea.text = string.Join("\n", newLog);
+        }
+    }
+
+    void Update()
+    {
+        
+        //Toggle visibility when tilde key pressed
+        if (Input.GetKeyDown("i"))
+        {
+            Debug.Log("Console activated!");
+            ToggleVisibility();
+        }
+        //Toggle visibility when 5 fingers touch.
+        if (Input.touches.Length == 5)
+        {
+            if (!didShow)
+            {
+                ToggleVisibility();
+                didShow = true;
+            }
+        }
+        else
+        {
+            didShow = false;
+        }
+    }
+
+    void ToggleVisibility()
+    {
+        SetVisibility(!ViewContainer.activeSelf);
+    }
+
+    void SetVisibility(bool visible)
+    {
+        ViewContainer.SetActive(visible);
+    }
+
+   
+    /// 
+    /// Event that should be called by anything wanting to submit the current input to the console.
+    /// 
+    public void RunCommand()
+    {
+        if (gamestate.executeStateCommand(inputField.text)) { return; }
+
+        console.RunCommandString(inputField.text);
+        
+        inputField.text = "";
+    }
+
+}
--- ouroboros/assets/scripts/Helper/GameStateController.cs
+++ ouroboros/assets/scripts/Helper/GameStateController.cs
@@ -1,69 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class GameStateController : MonoBehaviour {
-
-     
-	event_script_A roomA;
-	event_script_R1 room1;
-	event_script_R2 room2;
-	event_script_R3 room3;
-	event_script_R4 room4;
-	event_script_R5 room5;
-	player_script player;
+public class GameStateController : MonoBehaviour
+{
+    
+	private player_script player;
 	private GameObject playerCameraObject;
-	public Camera playerCamera;
+	private Camera playerCamera;
+    private Eventscript[] Room;
 
-
-    Eventscript[] Room;
-
-	void Start () {
-
+    private List<GameObject> listeners = new List<GameObject>();
+    
+	void Start ()
+    {
         Debug.developerConsoleVisible = true;
-        
-
-        roomA = GameObject.FindObjectOfType (typeof(event_script_A)) as event_script_A;
-		room1 = GameObject.FindObjectOfType (typeof(event_script_R1)) as event_script_R1;
-		room2 = GameObject.FindObjectOfType (typeof(event_script_R2)) as event_script_R2;
-		room3 = GameObject.FindObjectOfType (typeof(event_script_R3)) as event_script_R3;
-		room4 = GameObject.FindObjectOfType (typeof(event_script_R4)) as event_script_R4;
-		room5 = GameObject.FindObjectOfType (typeof(event_script_R5)) as event_script_R5;
 
 		player = GameObject.FindObjectOfType (typeof(player_script)) as player_script;
 		playerCameraObject = GameObject.FindWithTag ("MainCamera");
 		playerCamera = playerCameraObject.GetComponent<Camera> ();
 
-
         Room = new Eventscript[6];
-        Room[0] = roomA;
-        Room[1] = room1;
-        Room[2] = room2;
-        Room[3] = room3;
-        Room[4] = room4;
-        Room[5] = room5;
+        Room[0] = GameObject.FindObjectOfType(typeof(event_script_A)) as event_script_A;
+        Room[1] = GameObject.FindObjectOfType(typeof(event_script_R1)) as event_script_R1;
+        Room[2] = GameObject.FindObjectOfType(typeof(event_script_R2)) as event_script_R2;
+        Room[3] = GameObject.FindObjectOfType(typeof(event_script_R3)) as event_script_R3;
+        Room[4] = GameObject.FindObjectOfType(typeof(event_script_R4)) as event_script_R4;
+        Room[5] = GameObject.FindObjectOfType(typeof(event_script_R5)) as event_script_R5;
 
+        AddListener(gameObject);
+        AddListener(GameObject.Find("player"));
+        AddListener(GameObject.Find("EventManager_Anfang"));
+        AddListener(GameObject.Find("EventManager_R1"));
+        AddListener(GameObject.Find("EventManager_R2"));
+        AddListener(GameObject.Find("EventManager_R3"));
+        AddListener(GameObject.Find("EventManager_R4"));
+        AddListener(GameObject.Find("EventManager_R5"));
+        AddListener(GameObject.Find("EventManager_Plateau"));
 
+    }
+    public void AddListener(GameObject listener) {
+        if (!listeners.Contains(listener)) {
+            listeners.Add(listener);
+        }
+    }
     
+    public bool executeStateCommand(string input)
+    {
+        if (input.Equals("help")) {
+            
+        }
+
+        string[] parts = input.Split(new char[] {'.', '(', ')' }, 4);
+        if (parts == null) { return false; }
+        if (listeners.Where(obj => obj.name == parts[0]) == null) { return false; }
+        GameObject go = listeners.Where(obj => obj.name == parts[0]).SingleOrDefault();
+        if (go != null)
+        {
+            Debug.Log("Receiver object found: " + go.name);
+            go.SendMessage(parts[1], parts[2]);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public void CreateSphere(string input)
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Debug.Log(""+input);
+    }
 
 
-	}
-	
+
+
 	void Update () {
 
 		//size calculation utility variables:
 		
-
-
-
-
-
-
-
-
-
-
-
+        
 
 	}
 
